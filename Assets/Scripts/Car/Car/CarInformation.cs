6442d8d751e1ce40d9fa92c99a5024c15b66ae4a
Volodymyr Chernyavskyi <vovikche@gmail.com>
2024-07-04 11:51:04 +0300

    feat (scripts): car information script contain variables for display car information (in string)

--- Assets/Scripts/Car/Car/CarInformation.cs
+++ Assets/Scripts/Car/Car/CarInformation.cs
@@ -1,11 +1,30 @@
+using Cars_Racing.Vehicle.Transmission;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Cars_Racing.Vehicle.Car
 {
-    public static class CarInformation
+    public class CarInformation : MonoBehaviour
     {
-        public static CarConfiguration CarConfiguration = new CarConfiguration() { HorsePower = 200 };
+        [SerializeField]
+        private Rigidbody _rigidbody;
+
+        [SerializeField]
+        private Gearbox _gearbox;
+
+        [SerializeField][TextArea]
+        private string _carInformation;
+
+        private void Update()
+        {
+            GetInformation();
+        }
+
+        private void GetInformation()
+        {
+            _carInformation = $"Gear: {_gearbox.CurrentGear}\n" +
+                $"Speed: {(_rigidbody.velocity.magnitude * 3.6f).ToString("#.#")}Km/H";
+        }
     }
 }
