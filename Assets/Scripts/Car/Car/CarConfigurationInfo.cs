b64c78ef0d21ce612c31f29ce1d3a7ea4d0d0e60
Volodymyr Chernyavskyi <vovikche@gmail.com>
2024-07-04 11:48:53 +0300

    feat (scripts): created struct of car configuration

---
+++ Assets/Scripts/Car/Car/CarConfigurationInfo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cars_Racing.Vehicle.Car
+{
+    public static class CarConfigurationInfo
+    {
+        public static CarConfiguration CarConfiguration = new CarConfiguration() { HorsePower = 800 };
+    }
+}
