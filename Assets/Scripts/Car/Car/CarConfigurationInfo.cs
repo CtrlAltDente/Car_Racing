9bac3aaed71a71ca4b37e05843d562ac1c361771
Volodymyr Chernyavskyi <vovikche@gmail.com>
2024-07-04 15:54:16 +0300

    feat (scripts): changed horse power value

--- Assets/Scripts/Car/Car/CarConfigurationInfo.cs
+++ Assets/Scripts/Car/Car/CarConfigurationInfo.cs
@@ -6,6 +6,6 @@ namespace Cars_Racing.Vehicle.Car
 {
     public static class CarConfigurationInfo
     {
-        public static CarConfiguration CarConfiguration = new CarConfiguration() { HorsePower = 800 };
+        public static CarConfiguration CarConfiguration = new CarConfiguration() { HorsePower = 180 };
     }
 }
