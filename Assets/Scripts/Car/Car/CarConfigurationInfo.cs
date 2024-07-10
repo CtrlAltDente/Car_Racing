575f639c3035e5e22e321f288029029440e07b61
Volodymyr Chernyavskyi <vovikche@gmail.com>
2024-07-10 17:45:22 +0300

    feat (scripts): some changes in Car info

--- Assets/Scripts/Car/Car/CarConfigurationInfo.cs
+++ Assets/Scripts/Car/Car/CarConfigurationInfo.cs
@@ -9,10 +9,10 @@ namespace Cars_Racing.Vehicle.Car
         public static CarConfiguration CarConfiguration = new CarConfiguration()
         {
             MinRPM = 800,
-            MaxRPM = 7000,
+            MaxRPM = 9000,
             MaxSpeed = 180,
-            TopGear = 6,
-            HorsePower = 600 
+            TopGear = 5,
+            HorsePower = 180 
         };
 
         public static float AverageGearSpeed => CarConfiguration.MaxSpeed / CarConfiguration.TopGear;
