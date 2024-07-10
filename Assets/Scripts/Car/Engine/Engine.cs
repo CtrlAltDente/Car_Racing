64ff6aa0694686afc41710db9ef9f19bc7297c46
Volodymyr Chernyavskyi <vovikche@gmail.com>
2024-07-10 17:46:04 +0300

    feat (scripts): improved logic of calculating potential rpm and current rpm

--- Assets/Scripts/Car/Engine/Engine.cs
+++ Assets/Scripts/Car/Engine/Engine.cs
@@ -17,6 +17,7 @@ namespace Cars_Racing.Vehicle.EngineLogic
         [SerializeField]
         private string _information;
 
+        public float PotentialRPM { get; private set; }
         public float CurrentRPM { get; private set; }
         public float CurrentSpeed { get { return _wheelColliders.GetSpeed(); } }
         public float MotorTorgue { get; private set; }
@@ -29,8 +30,6 @@ namespace Cars_Racing.Vehicle.EngineLogic
 
         private void Gas(float gasValue)
         {
-            MotorTorgue = 0;
-
             if (!_gearbox.IsGearSwitching && !_gearbox.IsNeutralGear && gasValue != 0)
             {
 
@@ -40,11 +39,14 @@ namespace Cars_Racing.Vehicle.EngineLogic
                 gasValue = 0;
             }
 
+            CalculatePotentialRPM();
+            CalculateCurrentRPM(gasValue);
+
             MotorTorgue = CalculateMotorTorque(gasValue);
 
             _wheelColliders.DoWheelAction(wheel => wheel.motorTorque = MotorTorgue);
 
-            _information = $"Speed: {CurrentSpeed}, RPM: {CurrentRPM}, Motor Toqgue: {MotorTorgue}";
+            _information = $"Speed: {CurrentSpeed}, Current RPM: {CurrentRPM.ToString("#")}, Potential RPM: {PotentialRPM.ToString("#")}, Motor Toqgue: {MotorTorgue.ToString("#")}";
         }
 
         private void FootBreak(float breakValue)
@@ -52,23 +54,31 @@ namespace Cars_Racing.Vehicle.EngineLogic
             breakValue = 2;
         }
 
+        private void CalculatePotentialRPM()
+        {
+            float avgGearSpeed = CarConfigurationInfo.AverageGearSpeed;
+            float currentGearMinSpeed = !_gearbox.IsNeutralGear ? (avgGearSpeed * (_gearbox.CurrentGear - 2)) : 0;
+            float currentGearMaxSpeed = !_gearbox.IsNeutralGear ? (avgGearSpeed * _gearbox.CurrentGear) : 0;
+            float currentGearRPM = !_gearbox.IsNeutralGear ? Mathf.Lerp(0, 1f, (CurrentSpeed - currentGearMinSpeed) / (currentGearMaxSpeed - currentGearMinSpeed)) : 0;
+
+            PotentialRPM = Mathf.Clamp(Mathf.Lerp(0, CarConfigurationInfo.CarConfiguration.MaxRPM, currentGearRPM), CarConfigurationInfo.CarConfiguration.MinRPM, CarConfigurationInfo.CarConfiguration.MaxRPM);
+        }
+
+        private void CalculateCurrentRPM(float gasValue)
+        {
+            float stepRpm = CarConfigurationInfo.CarConfiguration.MaxRPM * Time.deltaTime;
+            CurrentRPM = Mathf.Clamp(CurrentRPM -stepRpm + 2 * stepRpm * Mathf.Abs(gasValue), CarConfigurationInfo.CarConfiguration.MinRPM, PotentialRPM);
+        }
+
         private float CalculateMotorTorque(float gasValue)
         {
-            float motorTorque = 0;
-
-            float avgGearSpeed = CarConfigurationInfo.AverageGearSpeed;
-            float currentGearMinSpeed = !_gearbox.IsNeutralGear ? (avgGearSpeed * (_gearbox.CurrentGear - 2) * 1.2f) : 0;
-            float currentGearMaxSpeed = !_gearbox.IsNeutralGear ? (avgGearSpeed * _gearbox.CurrentGear * 1.2f) : 0;
-            float currentGearRPM = !_gearbox.IsNeutralGear ? Mathf.Lerp(0, 1f, (CurrentSpeed - currentGearMinSpeed) / (currentGearMaxSpeed - currentGearMinSpeed)) : 0;
             float rpmGearPower = CarConstants.RPMPowerToGearSpeedCurve.Evaluate(CurrentRPM / CarConfigurationInfo.CarConfiguration.MaxRPM);
-
-            CurrentRPM = Mathf.Clamp(Mathf.Lerp(0, CarConfigurationInfo.CarConfiguration.MaxRPM, currentGearRPM), CarConfigurationInfo.CarConfiguration.MinRPM, CarConfigurationInfo.CarConfiguration.MaxRPM);
 
             float gearMultiplier = CarConstants.MotorTorgueGearCurve.Evaluate(Mathf.Abs(_gearbox.CurrentGear) / CarConfigurationInfo.CarConfiguration.TopGear);
 
             float torque = (CarConfigurationInfo.CarConfiguration.HorsePower * 745.7f) / ((2 * Mathf.PI * CurrentRPM) / 60);
 
-            motorTorque = gasValue * rpmGearPower * torque * gearMultiplier * Mathf.Lerp(0, 1, currentGearRPM);
+            float motorTorque = gasValue * rpmGearPower * torque * gearMultiplier;
 
             return motorTorque;
         }
