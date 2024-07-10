4142a7612756dc6322513288cf852839555c5a51
Volodymyr Chernyavskyi <vovikche@gmail.com>
2024-07-10 17:47:14 +0300

    feat (scripts): created script of speedometer

---
+++ Assets/Scripts/Car/Speedometer/Speedometer.cs
@@ -0,0 +1,26 @@
+using Cars_Racing.Vehicle.EngineLogic;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cars_Racing.Vehicle.SpeedometerLogic
+{
+    public class Speedometer : MonoBehaviour
+    {
+        [SerializeField]
+        private RectTransform _pointer;
+
+        [SerializeField]
+        private Engine _engine;
+
+        private void Update()
+        {
+
+        }
+
+        private void ShowSpeed()
+        {
+            
+        }
+    }
+}
