f588d39174d31a1f76effa739a4bba054d69431d
Volodymyr Chernyavskyi <vovikche@gmail.com>
2024-07-11 14:18:12 +0300

    feat (scripts): created basic logic of suspension spring target value change at speed change

---
+++ Assets/Scripts/Car/Suspension/WheelsSuspensionSpring.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cars_Racing.Vehicle.Suspension
+{
+    public class WheelsSuspensionSpring : MonoBehaviour
+    {
+        [SerializeField]
+        private WheelCollider[] _wheels;
+
+        [SerializeField]
+        private float _defaultValue;
+        [SerializeField]
+        private float _targetValue;
+
+        private float SpringTargetValue => Mathf.Lerp(_defaultValue, _targetValue,_wheels.GetSpeed() / (Car.CarConfigurationInfo.CarConfiguration.MaxSpeed/2));
+
+        private void Update()
+        {
+            ConfigureTargetValue();
+        }
+
+        private void ConfigureTargetValue()
+        {
+            _wheels.DoWheelAction(wheel => SetSuspensionSpringTargetValue(wheel, SpringTargetValue));
+        }
+
+        private void SetSuspensionSpringTargetValue(WheelCollider wheelCollider, float value)
+        {
+            var suspensionSpring = wheelCollider.suspensionSpring;
+            suspensionSpring.targetPosition = value;
+            wheelCollider.suspensionSpring = suspensionSpring;
+        }
+    }
+}
