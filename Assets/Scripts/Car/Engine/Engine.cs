132579759a9946d7c931aac6cad9b2af1d62cce9
Volodymyr Chernyavskyi <vovikche@gmail.com>
2024-07-12 12:09:45 +0300

    feat (scripts): added logic of neutral gear, made rpm displaying more smoothless

--- Assets/Scripts/Car/Engine/Engine.cs
+++ Assets/Scripts/Car/Engine/Engine.cs
@@ -35,7 +35,7 @@ namespace Cars_Racing.Vehicle.EngineLogic
 
         private void Gas(float gasValue)
         {
-            if (_gearbox.IsGearSwitching || _gearbox.IsNeutralGear)
+            if (_gearbox.IsGearSwitching)
             {
                 gasValue = 0;
             }
@@ -54,51 +54,42 @@ namespace Cars_Racing.Vehicle.EngineLogic
             PotentialRPM = Mathf.Clamp(Mathf.Lerp(0, CarConfigurationInfo.CarConfiguration.MaxRPM, CalculateSpeedPower()), CarConfigurationInfo.CarConfiguration.MinRPM, CarConfigurationInfo.CarConfiguration.MaxRPM);
 
             float stepRpm = CarConfigurationInfo.CarConfiguration.MaxRPM * Time.deltaTime;
-            CurrentRPM = Mathf.Clamp(CurrentRPM - stepRpm + 2 * stepRpm * Mathf.Abs(gasValue), CarConfigurationInfo.CarConfiguration.MinRPM, PotentialRPM);
+            float rpm = Mathf.MoveTowards(CurrentRPM, PotentialRPM, stepRpm);
+
+            CurrentRPM = gasValue > 0 ? rpm : Mathf.Clamp(CurrentRPM - stepRpm + 2 * stepRpm * Mathf.Abs(gasValue), CarConfigurationInfo.CarConfiguration.MinRPM, CarConfigurationInfo.CarConfiguration.MaxRPM);
         }
 
         private float CalculateMotorTorque(float gasValue)
         {
             float rpmGearPower = CarConstants.RPMPower.Evaluate(CurrentRPM / CarConfigurationInfo.CarConfiguration.MaxRPM);
-
             float rpmGearSpeedPower = CarConstants.RPMPowerToGearSpeedCurve.Evaluate(CalculateSpeedPower());
+
+            float torque = (CarConfigurationInfo.CarConfiguration.HorsePower * 745.7f) / ((2 * Mathf.PI * CurrentRPM) / 60);
+            float movement = Mathf.Clamp(_gearbox.CurrentGear, -1, 1);
 
             float gearMultiplier = CarConstants.MotorTorgueGearCurve.Evaluate(Mathf.Abs(_gearbox.CurrentGear) / CarConfigurationInfo.CarConfiguration.TopGear);
 
-            float torque = (CarConfigurationInfo.CarConfiguration.HorsePower * 745.7f) / ((2 * Mathf.PI * CurrentRPM) / 60);
-
-            float movement = Mathf.Clamp(_gearbox.CurrentGear, -1, 1);
-
-            float motorTorque = gasValue * ((rpmGearPower + rpmGearSpeedPower)) * torque * (1 - CalculateSpeedPower()) * movement;
-
-            return motorTorque;
+            return gasValue * ((rpmGearPower + rpmGearSpeedPower)) * torque * (1 - CalculateSpeedPower()) * movement;
         }
 
         private float CalculateSpeedPower()
         {
-            float potentialGearRpm = 0;
-
-            if (!_gearbox.IsNeutralGear)
+            if (_gearbox.CurrentGear < 0)
             {
-                if (_gearbox.CurrentGear < 0)
-                {
-                    potentialGearRpm = Mathf.Lerp(0, 1, Mathf.Abs(CurrentSpeed - 0) / ((Mathf.Abs(CurrentGearMaxSpeed) - 0)));
-                }
-                else if (CurrentSpeed < CurrentGearMinSpeed)
-                {
-                    potentialGearRpm = 0;
-                }
-                else if (CurrentSpeed > CurrentGearMaxSpeed)
-                {
-                    potentialGearRpm = 1;
-                }
-                else
-                {
-                    potentialGearRpm = Mathf.Lerp(0, 1, (CurrentSpeed - CurrentGearMinSpeed) / (CurrentGearMaxSpeed - CurrentGearMinSpeed));
-                }
+                return Mathf.Lerp(0, 1, Mathf.Abs(CurrentSpeed - 0) / ((Mathf.Abs(CurrentGearMaxSpeed) - 0)));
             }
-
-            return potentialGearRpm;
+            else if (CurrentSpeed < CurrentGearMinSpeed)
+            {
+                return 0;
+            }
+            else if (CurrentSpeed > CurrentGearMaxSpeed)
+            {
+                return 1;
+            }
+            else
+            {
+                return Mathf.Lerp(0, 1, (CurrentSpeed - CurrentGearMinSpeed) / (CurrentGearMaxSpeed - CurrentGearMinSpeed));
+            }
         }
 
         private void FootBreak(float breakValue)
