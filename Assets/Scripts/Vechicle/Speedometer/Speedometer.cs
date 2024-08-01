ec4312c6c1e7c1663a6eafb2b2b90cb31707e55f
Volodymyr Chernyavskyi <vovikche@gmail.com>
2024-08-01 13:06:54 +0300

    feat (scripts): reworked logic of speedometer, removed RpmMeter

--- Assets/Scripts/Vechicle/Speedometer/Speedometer.cs
+++ Assets/Scripts/Vechicle/Speedometer/Speedometer.cs
@@ -1,6 +1,9 @@
+using Cars_Racing.Vehicle.Car;
 using Cars_Racing.Vehicle.EngineLogic;
+using Cars_Racing.Vehicle.Transmission;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace Cars_Racing.Vehicle.SpeedometerLogic
@@ -11,16 +14,38 @@ namespace Cars_Racing.Vehicle.SpeedometerLogic
         private RectTransform _pointer;
 
         [SerializeField]
+        private TextMeshProUGUI _speedText;
+        [SerializeField]
+        private TextMeshProUGUI _geartext;
+
+        [SerializeField]
         private Engine _engine;
+        [SerializeField]
+        private Gearbox _gearbox;
 
         private void Update()
         {
+            ShowRpm();
+            ShowSpeed();
+            ShowGear();
+        }
 
+        private void ShowRpm()
+        {
+            float rpmEfficient = _engine.CurrentRPM / CarConfigurationInfo.CarConfiguration.MaxRPM;
+
+            _pointer.rotation = Quaternion.Slerp(Quaternion.Euler(0, 0, 140), Quaternion.Euler(0, 0, -30), rpmEfficient);
         }
 
         private void ShowSpeed()
         {
-            
+            _speedText.text = (Mathf.Abs((int)_engine.CurrentSpeed)).ToString("#####");
+        }
+
+        private void ShowGear()
+        {
+            string gearText = _gearbox.CurrentGear < 0 ? "R" : _gearbox.CurrentGear == 0 ? "N" : _gearbox.CurrentGear.ToString("##");
+            _geartext.text = gearText;
         }
     }
 }
