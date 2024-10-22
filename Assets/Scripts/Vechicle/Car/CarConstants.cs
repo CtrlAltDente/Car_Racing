fce96c57acef6ac4e6edb9d672891c254f18f847
Volodymyr Cherniavskyi <vovikche@gmail.com>
2024-10-22 12:08:36 +0300

    chore (scripts): increase end value of MotorTorque, fix variable name

--- Assets/Scripts/Vechicle/Car/CarConstants.cs
+++ Assets/Scripts/Vechicle/Car/CarConstants.cs
@@ -6,7 +6,7 @@ namespace Cars_Racing.Vehicle.Car
 {
     public static class CarConstants
     {
-        public static AnimationCurve MotorTorgueGearCurve = new AnimationCurve(new Keyframe(0, 1), new Keyframe(1, 0.2f));
+        public static AnimationCurve MotorTorqueGearCurve = new AnimationCurve(new Keyframe(0, 1), new Keyframe(1, 0.6f));
         
         public static AnimationCurve RPMPowerToGearSpeedCurve = new AnimationCurve(new Keyframe(0, 0f), new Keyframe(0.7f, 1), new Keyframe(1, 0));
         public static AnimationCurve RPMPower = new AnimationCurve(new Keyframe(0, 0), new Keyframe(0.5f, 1), new Keyframe(1, 0));
