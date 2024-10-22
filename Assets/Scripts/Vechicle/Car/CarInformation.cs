0d7e230b7f62ddb2c687f73d4ad8302ea4e2b98d
Volodymyr Cherniavskyi <vovikche@gmail.com>
2024-10-22 12:09:23 +0300

    feat (scripts): add values of wheels speed to CarInformation

--- Assets/Scripts/Vechicle/Car/CarInformation.cs
+++ Assets/Scripts/Vechicle/Car/CarInformation.cs
@@ -13,7 +13,11 @@ namespace Cars_Racing.Vehicle.Car
         [SerializeField]
         private Gearbox _gearbox;
 
-        [SerializeField][TextArea]
+        [SerializeField]
+        private WheelCollider[] _wheelColliders;
+
+        [SerializeField]
+        [TextArea(10,100)]
         private string _carInformation;
 
         private void Update()
@@ -23,8 +27,29 @@ namespace Cars_Racing.Vehicle.Car
 
         private void GetInformation()
         {
-            _carInformation = $"Gear: {_gearbox.CurrentGear}\n" +
-                $"Speed: {(_rigidbody.velocity.magnitude * 3.6f).ToString("#.#")}Km/H";
+            _carInformation = GetCurrentGear() + GetSpeed() + GetMotorTorque();
+            ;
+        }
+
+        private string GetCurrentGear()
+        {
+            return $"Gear: {_gearbox.CurrentGear}\n";
+        }
+
+        private string GetSpeed()
+        {
+            return $"Speed: {(_rigidbody.velocity.magnitude * 3.6f).ToString("#.#")}Km/H\n";
+        }
+
+        private string GetMotorTorque()
+        {
+            string wheelsMotorTorque = string.Empty;
+            for (int i = 1; i <= _wheelColliders.Length; i++)
+            {
+                wheelsMotorTorque += $"Wheel {1} motor torque: {_wheelColliders[i-1].motorTorque}\n";
+            }
+
+            return wheelsMotorTorque;
         }
 
         private void OnDrawGizmos()
