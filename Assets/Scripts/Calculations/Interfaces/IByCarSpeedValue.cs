5a2554984fecff61d675b2ac77ba0792f1c05566
Volodymyr Cherniavskyi <vovikche@gmail.com>
2024-10-22 12:04:52 +0300

    feat (scripts): create interface of ICarSpeedValue for configuration some values by car speed

---
+++ Assets/Scripts/Calculations/Interfaces/IByCarSpeedValue.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cars_Racing.Calculations.Interfaces
+{
+    public interface IByCarSpeedValue
+    {
+        float ByCarSpeedValue { get; }
+    }
+}
